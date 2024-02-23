032e2401f09df4e8d39dab82cfcb3c2afcadcadf
Skurrrrr <wirelessdan@proton.me>
2024-02-22 20:41:22 -0800

    chilling with Leena

--- notes.md
+++ notes.md
@@ -8,3 +8,15 @@ Gloss over IP subnet stuff
  - use bgp.he.net to get company subnets
  - use tunnelsup.com/subnet-calculator to calculate subnets
  - use whois.com to see... who is
+
+Install jp:
+sudo apt-get install jq
+Then run this to parse ip addresses from gstatic.com into a text file: 
+ #had to change a ' to "
+ 
+ curl -s https://www.gstatic.com/ipranges/cloud.json | jq -r ".prefixes[] | select(.ipv4Prefix != null) | .ipv4Prefix" > googleIPs.txt
+
+Run this to get AWS ips:
+#I had to look up where to get the IP json, the class link didn't work
+
+curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.prefixes[].ip_prefix' > AWS.txt
--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,16 +1,51 @@
+import re #regular expression
 import os
 import subprocess
+import asyncio #make sure to install asyncio. I used: pip3 install asyncio --break-system-package
+import aiohttp #module to make parallel requests
 
 class SSLChecker:
     def __init__(
         self,
+        ssl_port=443,
         mass_scan_results_file="massscanResults.txt", 
         ips_file="ips.txt", 
-        masscan_rate=10000
+        masscan_rate=10000,
+        timeout=2,
+        chunkSize=2000
+
     ):
+        self.ssl_port=ssl_port
         self.mass_scan_results_file=mass_scan_results_file
         self.ips_file = ips_file
         self.masscan_rate = masscan_rate #defines how fast to run the scan, 10,000 is ideal
+        self.timeout=timeout
+        self.chunkSize=chunkSize
+
+
+    async def fetch_certificates(self, ip):
+        try:
+            cert= ssl.
+        except Exception as e:
+            print(f"Error for {ip}: {e}")
+
+    async def extract_domains():
+        try:
+            with open(self.mass_scan_results_file,"r") as file:
+                content=file.read()
+
+            ip_pattern = r"\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}"
+            ip_addresses = re.findall(ip_pattern, content)
+
+            for i in range(0,len(ip_addresses),self.chunkSize):
+                async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=self.MAX_CONCURRENT,ssl=False)) as session:
+                    chunk_Of_IPs = ip_addresses[i:i+self.chunkSize]
+                    ip_and_common_names=[]
+
+                    ip_and_common_names = await asyncio.gather(*[self.fetch_certificates(ip) for ip in chunk_Of_IPs])
+
+
+
 
     def run_masscan(self): # p443=port, rate=rate, wait 0 =don't wait on timeout, -iL=output -oH=where to save results
         try:
@@ -35,10 +70,10 @@ class SSLChecker:
                 print(f'File "{file_path}" has been created')
 
 
-    def main(self):
+    async def main(self):
         self.check_and_create_files(self.mass_scan_results_file, self.ips_file)
         self.run_masscan()
-
+        await self.extract_domains() #uses an asyncronous method, hence "await"
 
 if __name__=="__main__":
     ssl_checker = SSLChecker()
